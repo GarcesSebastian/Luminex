2aba9eeae825f2dfaa218f9d804cf641b6519b35
soyvillareal <sebastiangarces152@gmail.com>
2024-06-03 20:33:10 -0500

    I did add server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+//Estructura basica de node con express de prueba usando import
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
