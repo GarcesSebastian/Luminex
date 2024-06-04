84085c22c5c3e8815015f3516a056ea13457f8b6
soyvillareal <soyvillareal@gmail.com>
2024-06-03 21:55:32 -0500

    Test

--- server/index.js
+++ server/index.js
@@ -4,26 +4,26 @@ const multer = require('multer');
 const ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
 const fs = require('fs');
-// const database = require('./database/database');
+const database = require('./database/database');
 
 const app = express();
 const port = 4000;
 
-// app.use(bodyParser.json({ limit: '500mb' }));
-// app.use(bodyParser.urlencoded({ extended: true, limit: '500mb' }));
+app.use(bodyParser.json({ limit: '500mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '500mb' }));
 
-// app.use((req, res, next) => {
-//     res.setHeader('Content-Type', 'multipart/form-data');
-//     req.headers['content-length'] = '524288000'; 
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'multipart/form-data');
+    req.headers['content-length'] = '524288000'; 
+    next();
+});
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
 app.get('/words', async (req, res) => {
-    const connection =false;
+    const connection = false;
 
     if (!connection) {
         return res.json({ message: 'Not connected to the database' });
