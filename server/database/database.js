9bae0a4b98a4c8c86a43e68c2960b4f8b080f485
soyvillareal <soyvillareal@gmail.com>
2024-06-03 22:05:11 -0500

    Test

--- server/database/database.js
+++ server/database/database.js
@@ -1,12 +1,12 @@
-// const mysql = require("promise-mysql");
+const mysql = require("promise-mysql");
 
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     database: "luminex",
-//     user: "root",
-//     password: "",
-// });
+const connection = mysql.createConnection({
+    host: "localhost",
+    database: "luminex",
+    user: "root",
+    password: "",
+});
 
-// const getConnection = async () => await connection;
+const getConnection = async () => await connection;
 
-// module.exports = { getConnection };
+module.exports = { getConnection };
